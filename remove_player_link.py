0ce7918d7fad6bb761d5a973dc16b011551518f3
iSpoonz <ispoonz.codgamepedia@gmail.com>
2020-06-12 21:22:14 +0100

    remove player link from templates

---
+++ remove_player_link.py
@@ -0,0 +1,23 @@
+from river_mwclient.esports_client import EsportsClient
+from river_mwclient.auth_credentials import AuthCredentials
+from river_mwclient.template_modifier import TemplateModifierBase
+
+credentials = AuthCredentials(user_file="bot")
+site = EsportsClient('cod-esports', credentials=credentials)  # Set wiki
+summary = 'Use just |player=, no |link='  # Set summary
+
+
+class TemplateModifier(TemplateModifierBase):
+    def update_template(self, template):
+        if not template.has('link'):
+            return
+        player = template.get('player').value.strip()
+        link = template.get('link').value.strip()
+        if not link.lower().startswith(player.lower()):
+            return
+        template.remove('link')
+        template.add('player', link)
+
+
+TemplateModifier(site, 'TeamRoster/Line',
+                 summary=summary).run()
